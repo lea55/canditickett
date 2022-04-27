afc97a558b08d64d116934f14f8fea51b962a820
lea55 <leandrovqz@hotmail.com>
2022-04-27 07:59:28 -0300

    d
    d

--- android/app/src/main/kotlin/com/example/canditickets/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.canditickets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/listacategorias.dart
+++
@@ -1,7 +0,0 @@
-const List listacategorias = [
-  {"name": "Concierto", "img": 'assets/img4.jpg'},
-  {"name": "Discotecas", "img": 'assets/img3.jpg'},
-  {"name": "Bares", "img": 'assets/img2.jpg'},
-  {"name": "Pub", "img": 'assets/img1.jpg'},
-  {"name": "Cines", "img": 'assets/web.jpg'},
-];
